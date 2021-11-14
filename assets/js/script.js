939f6cc904e69b134f84dddbf9092365727ff4b4
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-11-13 18:52:11 -0800

    search on-click function with html details

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var testCity = "Sacramento"
 var myKey = "b7cd031e64f6a5980262c7acabdf2831"
 var url = "https://api.openweathermap.org/data/2.5/weather?q=" + testCity + "&appid=" + myKey + "&units=imperial"
 var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + testCity + "&appid=" + myKey + "&units=imperial"
-
+$(".search").on("click", function(event) {
+    event.preventDefault()
 
 
 
@@ -37,3 +38,4 @@ fetch(forecast)
 
     })
 
+})
