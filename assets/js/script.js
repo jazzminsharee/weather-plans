5f2a4d594d2f548188ab3d5cf3ba9a13fbe27786
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-11-13 16:23:38 -0800

    setup initial files structure with variables for API

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var city = $("#cityName").val()
+var testCity = "Sacramento"
+var myKey = "b7cd031e64f6a5980262c7acabdf2831"
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + testCity + "&appid=" + myKey + "&units=imperial"
+var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + testCity + "&appid=" + myKey + "&units=imperial"
+
+
